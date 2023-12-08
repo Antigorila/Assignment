9c75f192e587c372da1c25da011daef4db23d818
brovinszkiandras <brovinszki.andras@gmail.com>
2023-12-08 21:57:55 +0100

    Összekapcsolás

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'assignment_title' => fake()->word(),
+            'assignment' => fake()->sentence(),
+            'category_id' => fake()->numberBetween(1,10),
+        ];
+    }
+}
