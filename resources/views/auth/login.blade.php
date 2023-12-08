69097c84057556207afb7f2bbfa686d8483d631c
Antigorila <antigorilla.666@gmail.com>
2023-12-08 17:30:18 +0100

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,29 +39,24 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class="container text-center">
+                            <div class="d-inline-block">
+                              <select class="form-select" name="" id="">
+                                <option selected disabled>Choose role</option>
+                                <option value="teacher">Teacher</option>
+                                <option value="student">Student</option>
+                              </select>
                             </div>
-                        </div>
+                          </div>
+                          
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                          <hr>
+
+                        <div class="row mb-0 text-center">
+                            <div class="">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
